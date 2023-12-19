ad6e82c21289f3e000b8c95fd98cb182a82646cb
Júlio Kantarelly <127207301+juliokantarelly@users.noreply.github.com>
2023-12-19 15:10:44 -0300

    Create ambiente.js

---
+++ aula-13/ambiente.js
@@ -0,0 +1,26 @@
+console.log('Vai começar!')
+for (var f = 1; f <= 10; f++) {
+    console.log(`Passo ${f}`)
+}
+console.log('Fim.')
+
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 52)*/
+
+
+/*console.log('Tudo bem? Vou servir sua pizza')
+var fatias = 50
+function comerFatia() {
+    while (fatias > 1) {
+        console.log(`Restam ${fatias} fatias.`)
+        fatias--
+    }
+    console.log(`Resta ${fatias} fatia.`)
+    console.log('Acabou a pizza.')
+}
+if (fatias != 0) {
+    comerFatia()
+}*/
